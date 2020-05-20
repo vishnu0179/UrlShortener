4fd273a2d43619f51c8692a2dbffc7a32ad10547
vishnu0179 <vjvj140@gmail.com>
2020-05-20 06:23:58 +0530

    Added docker compose and docker file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const db = require('./models/shortDB')
 
 const port = process.env.PORT || 5000;;
 
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/urlShortener', {
+mongoose.connect(process.env.MONGODB_URI||'mongodb://mongo:27017/urlShortener', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
